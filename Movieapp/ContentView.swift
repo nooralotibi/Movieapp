e104b9a7619e0afa2d4c93728412fbc4dfe7c701
nooreealotibi <nooreealotibi@gmail.com>
2024-02-12 21:42:32 +0300

    Initial Commit

---
+++ Movieapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movieapp
+//
+//  Created by noor alotibi on 02/08/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movieapp/MovieappApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieappApp.swift
+//  Movieapp
+//
+//  Created by noor alotibi on 02/08/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MovieappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
